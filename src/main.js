14d7bf9974c112c20f51909826ad3a70be751084
NHome <norbipascu92@gmail.com>
2020-03-31 15:37:32 +0300

    modifier

--- src/main.js
+++ src/main.js
@@ -1,20 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.directive('highlight',{
-  bind(el, binding, vnode){
+Vue.directive("highlight", {
+  bind(el, binding, vnode) {
     //el.style.backgroundColor = 'green';
     //el.style.backgroundColor = binding.value;
-    if(binding.arg == 'background'){
-      el.style.backgroundColor = binding.value
+    let delay = 0;
+    if (binding.modifiers["delayed"]) {
+      delay = 3000;
     }
-    else{
-      el.style.color = binding.value
-    }
+    setTimeout(() => {
+      if (binding.arg == "background") {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
   }
-})
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
